870302968252430f63b307089a6cb0d1f9d4d7e1
YT <>
2026-05-18 14:30:41 +0800

    check1: add parallel PriorityEncoder

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-run:
-	./mill Demo.runMain gcd.GCD
 check1:
 	./mill Demo.runMain pRed.Main
+run:
+	./mill Demo.runMain gcd.GCD
 clean:
 	rm -rf out *.sv *.f
--- src/main/scala/pRed/PRed.scala
+++ src/main/scala/pRed/PRed.scala
@@ -14,9 +14,14 @@ class ANDR extends Module {
   })
   io.out := io.in.andR
 }
+object ParallelAND {
+  def apply[T <: Data](xs: Seq[T]): T = {
+    ParallelOperation(xs, (a: T, b:T) => (a.asUInt & b.asUInt).asTypeOf(xs.head))
+  }
+}
 
 //question: is PriorityMux implementation is parallel? NO
-class PRIORITYMUX extends Module {
+class Check1PriorityMux extends Module {
   val io = IO(new Bundle {
     val inContrl = Input(UInt(8.W))
     val inData   = Input(Vec(8, UInt(32.W)))
@@ -25,9 +30,53 @@ class PRIORITYMUX extends Module {
   io.out := PriorityMux(io.inContrl, io.inData)
 }
 
+
+object ParallelOperation {
+  def apply[T](xs: Seq[T], func: (T, T) => T): T = {
+    require(xs.nonEmpty)
+    xs match {
+      case Seq(a) => a
+      case Seq(a, b) => func(a, b)
+      case _ =>
+        apply(Seq(apply(xs take xs.size/2, func), apply(xs drop xs.size/2, func)), func)
+    }
+  }
+}
+
+object ParallelPriorityMux {
+  def apply[T <: Data](in: Seq[(Bool, T)]): T = {
+    ParallelOperation(in, (a: (Bool, T), b: (Bool, T)) => (a._1 || b._1, Mux(a._1, a._2, b._2)))._2
+  }
+  def apply[T <: Data](sel: Bits, in: Seq[T]): T = apply((0 until in.size).map(sel(_)), in)
+  def apply[T <: Data](sel: Seq[Bool], in: Seq[T]): T = apply(sel zip in)
+}
+
+object PriorityMux {
+  def apply[T <: Data](in: Seq[(Bool, T)]): T = {ParallelPriorityMux(in)}
+  def apply[T <: Data](sel: Bits, in: Seq[T]): T = apply((0 until in.size).map(sel(_)), in)
+  def apply[T <: Data](sel: Seq[Bool], in: Seq[T]): T = apply(sel zip in)
+}
+
+//make a parallel PriorityEncoder
+object ParallelPriorityEncoder {
+  def apply(in: Seq[Bool]): UInt = ParallelPriorityMux(in, (0 until in.size).map(_.asUInt))
+  def apply(in: Bits): UInt = apply(in.asBools)
+}
+object PriorityEncoder {
+  def apply(in: Bits): UInt = ParallelPriorityEncoder(in)
+}
+class Check1PriorityEncoder extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(8.W))
+    val out = Output(UInt(3.W))
+  })
+  io.out := PriorityEncoder(io.in)
+}
+
+
 object Main extends App {
   ChiselStage.emitSystemVerilogFile(
-    new PRIORITYMUX,
+    new Check1PriorityEncoder,
     firtoolOpts = Array(
       "-disable-all-randomization", 
       "-strip-debug-info", 
